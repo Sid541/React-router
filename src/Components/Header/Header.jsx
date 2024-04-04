77e6846d149f91ebd2cd8e051da01976fd763ac3
Sid541 <144512053+Sid541@users.noreply.github.com>
2024-04-05 00:54:16 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import{Link, NavLink}from 'react-router-dom'
 export default function Header() {
     return (
         <header className='shadow sticky z-50 top-0'>
-            <nav className='bg-white border-red-200 px-4 lg:px-6 py-3'>
+            <nav className='bg-cyan-500 border-red-200 px-4 lg:px-6 py-3'>
                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <Link to="/" className='flex items-center'>
                 <img src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png" className='mr-3 h-12' alt="Logo" />
